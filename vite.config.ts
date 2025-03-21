de1254c4df34fe2debb2deb2b0dbfa9b2e96edb5
Deltaporto <rafael.aa.adv@gmail.com>
2025-03-21 20:04:48 -0300

    fix: altera base URL do Vite para usar caminhos relativos

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/judicial-vacation-optimizer/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
